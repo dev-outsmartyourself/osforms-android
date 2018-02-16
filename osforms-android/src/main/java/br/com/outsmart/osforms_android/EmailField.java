87d689d9980ad0b9a2764e49c47318825bde2c19
unknown <imancinelli.appsimples@gmail.com>
2018-02-16 18:33:53 -0200

    inicio da contrução do Emal Class

--- osforms-android/src/main/java/br/com/outsmart/osforms_android/EmailField.java
+++ osforms-android/src/main/java/br/com/outsmart/osforms_android/EmailField.java
@@ -1,7 +1,12 @@
 package br.com.outsmart.osforms_android;
 
 import android.content.Context;
+import android.inputmethodservice.Keyboard;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by kalunga on 16/02/2018.
@@ -9,9 +14,26 @@ import android.support.v7.widget.AppCompatEditText;
 
 public class EmailField extends AppCompatEditText {
 
-    public EmailField(Context context) {
+    public EmailField(final Context context) {
         super(context);
+        setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    isValidEmail(getText());
+                }
+            }
+        });
+    }
 
 
+    public static boolean isValidEmail(CharSequence target){
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
     }
 }
